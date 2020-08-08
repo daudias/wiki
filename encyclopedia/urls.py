9baf5c85311ea55627c4d6f7f0f795c4d85966c6
Dias <47509783+daudias@users.noreply.github.com>
2020-08-09 05:35:36 +0600

    made 4, 5, 6

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.page, name="page"),
-    path("search", views.search, name="search")
+    path("search", views.search, name="search"),
+    path("create", views.createNewPage, name="createNewPage"),
+    path("randomPage", views.randomPage, name="randomPage"),
+    path("wiki/<str:title>/edit", views.editPage, name="editPage")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import random
 
 from . import util
 
@@ -19,20 +20,38 @@ def page(request, title):
 		})
 
 def search(request):
-	if request.method == "POST":
-		query = request.POST.get("query")
-		entry = util.get_entry(query)
-		if entry != None:
-			return page(request, query)
-		else:
-			mathchedEntries = []
-			for entry in util.list_entries():
-				if query.lower() in entry.lower():
-					mathchedEntries.append(entry)
-			return render(request, "encyclopedia/search.html", {
-				"entries" : mathchedEntries
-				})
-	return render(request, "encyclopedia/error.html", {
-		"errorMessage" : "Not allowed (Access must be by POST method)"
+	query = request.POST.get("query")
+	entry = util.get_entry(query)
+	if entry != None:
+		return page(request, query)
+	mathchedEntries = []
+	for entry in util.list_entries():
+		if query.lower() in entry.lower():
+			mathchedEntries.append(entry)
+	return render(request, "encyclopedia/search.html", {
+		"entries" : mathchedEntries
 		})
+
+def createNewPage(request):
+	if request.method == "POST":
+		title = request.POST.get("title")
+		entry = util.get_entry(title)
+		if entry != None:
+			return render(request, "encyclopedia/error.html", {
+				"errorMessage" : "An entry with the title is already exist"
+				})
+		util.save_entry(title, request.POST.get("content"))
+		return page(request, title)
+	return render(request, "encyclopedia/create.html")
+
+def editPage(request, title):
+	if request.method == "POST":
+		util.save_entry(title, request.POST.get("content"))
+		return page(request, title)
+	return render(request, "encyclopedia/edit.html", {
+		"title" : title, "content" : util.get_entry(title)
+		})
+
+def randomPage(request):
+	return page(request, random.choice(util.list_entries()))
 
