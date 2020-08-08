fcd405b5e470c8ea3e4fecfbf39ae29c4f7d7a89
Dias <47509783+daudias@users.noreply.github.com>
2020-08-09 04:48:32 +0600

    made 1, 2, 3

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,31 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def page(request, title):
+	entry = util.get_entry(title)
+	if entry != None:
+		return render(request, "encyclopedia/page.html", {
+		"title" : title, "content" : entry 
+		})
+	return render(request, "encyclopedia/error.html", {
+		"errorMessage" : "No such entry"
+		})
+
+def search(request):
+	if request.method == "POST":
+		query = request.POST.get("query")
+		entry = util.get_entry(query)
+		if entry != None:
+			return page(request, query)
+		else:
+			mathchedEntries = []
+			for entry in util.list_entries():
+				if query.lower() in entry.lower():
+					mathchedEntries.append(entry)
+			return render(request, "encyclopedia/search.html", {
+				"entries" : mathchedEntries
+				})
+	return render(request, "encyclopedia/error.html", {
+		"errorMessage" : "Not allowed (Access must be by POST method)"
+		})
+
